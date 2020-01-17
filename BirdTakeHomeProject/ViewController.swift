7c3f4c562aef32775c7b8084d8b02c76ea8b7b67
Sergey Yelmanov <sergey.yelmanov@cygility.com>
2020-01-17 20:41:04 +0200

    Initial Commit

---
+++ BirdTakeHomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirdTakeHomeProject
+//
+//  Created by Sergey Yelmanov on 17.01.2020.
+//  Copyright © 2020 Sergey Yelmanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
